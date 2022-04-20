f3bd830ed5739b3c9e4f3b9dad450301f2b866d2
fdcrespi <fdcrespi@gmail.com>
2022-04-20 13:46:51 -0300

    ejercicio7-practico2 / faltan metodos - ARBOL CON NODO

---
+++ src/practico2/ejercicio7/ArbolBinarioWithNode.java
@@ -0,0 +1,67 @@
+package practico2.ejercicio7;
+
+import practico1.ejercicio8.Node;
+
+public class ArbolBinarioWithNode {
+    Node<Integer> root;
+
+    public ArbolBinarioWithNode(){
+        this.root = null;
+    }
+
+    private void setRoot(Node<Integer> nodo){
+        this.root = nodo;
+    }
+
+    public Integer getRoot(){
+        return this.root.getInfo();
+    }
+
+    public boolean hasElem(Integer value){
+        return isFound(root, value);
+    }
+
+    private boolean isFound(Node<Integer> nodo, Integer value){
+        if (nodo.getInfo().equals(value)) return true;
+        if (nodo.getInfo() < value){
+            if(nodo.getPrev() == null) return false;
+            return isFound(nodo.getPrev(), value);
+        }
+        if (nodo.getNext() == null) return false;
+        return isFound(nodo.getNext(), value);
+    }
+
+    public boolean isEmpty(){
+        return this.root == null;
+    }
+
+    public void insert(Integer value){
+        insertInOrder(root, value);
+    }
+
+    private void insertInOrder(Node<Integer> nodo, Integer value){
+        if (nodo == null) {
+            nodo.setInfo(value);
+        }
+        if(value < nodo.getInfo()){
+            if (nodo.getPrev() == null) nodo.setPrev(new Node<>(value));
+            else insertInOrder(nodo.getPrev(), value);
+        }
+        if (value > nodo.getInfo()){
+            if (nodo.getNext() == null) nodo.setNext(new Node<>(value));
+            else insertInOrder(nodo.getNext(), value);
+        }
+    }
+
+    public boolean delete(Integer value){
+        return deleteElement(root, value);
+    }
+
+    private boolean deleteElement(Node<Integer> nodo, Integer value){
+        //TODO
+        return false;
+    }
+}
+
+
+/*https://programmerclick.com/article/35711887866/*/
--- src/practico2/ejercicio7/Main.java
+++ src/practico2/ejercicio7/Main.java
@@ -5,11 +5,12 @@ public class Main {
         ArbolBinario AB = new ArbolBinario(10);
         AB.insert(3);
         AB.insert(6);
+        AB.insert(5);
         AB.insert(12);
         AB.insert(11);
         boolean delete = AB.delete(6);
         System.out.println(delete);
-        AB.insert(7);
+        /*AB.insert(7);*/
         AB.printInOrder();
     }
 }
