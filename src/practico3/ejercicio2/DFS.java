1b462fad9efaa0580c0595dd9ab0db2622633429
fdcrespi <fdcrespi@gmail.com>
2022-05-13 12:50:56 -0300

    Implementacion Practico 3 - ejercicio 2 DFS

---
+++ src/practico3/ejercicio2/DFS.java
@@ -0,0 +1,49 @@
+package practico3.ejercicio2;
+
+import practico3.ejercicio1.Grafo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class DFS {
+    private Grafo<?> grafo;
+    private HashMap<Integer, String> colores;
+    private int tiempo;
+    private HashMap<Integer, Integer> tiempos;
+    private HashMap<Integer, Integer> tiempos_fin;
+
+    public DFS(Grafo<?> grafo){
+        this.grafo = grafo;
+    }
+
+    public ArrayList<?> recorridoBFS(){
+        Iterator<Integer> vertices = this.grafo.obtenerVertices();
+        while (vertices.hasNext()){
+            int verticeId = vertices.next();
+            this.colores.put(verticeId, "BLANCO");
+        }
+        tiempo = 0;
+        while (vertices.hasNext()){
+            int v = vertices.next();
+            if (colores.get(v).equals("BLANCO")){
+                DFS_VISIT(v);
+            }
+        }
+        return null;
+    }
+
+    private void DFS_VISIT(int v) {
+        colores.replace(v, "AMARILLO");
+        tiempos.put(v, ++tiempo);
+        Iterator<Integer> adyacentes = this.grafo.obtenerAdyacentes(v);
+        while (adyacentes.hasNext()){
+            int u = adyacentes.next();
+            if (colores.get(u).equals("BLANCO")){
+                DFS_VISIT(u);
+            }
+        }
+        colores.replace(v, "NEGRO");
+        tiempos_fin.put(v, ++tiempo);
+    }
+}
