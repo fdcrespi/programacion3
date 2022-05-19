c933af23644314644fe293c9b6fc82b9fe4d9b1a
fdcrespi <fdcrespi@gmail.com>
2022-05-19 12:03:33 -0300

    Implementacion Practico 3 - ejercicio 6

--- src/practico3/ejercicio6/Main.java
+++ src/practico3/ejercicio6/Main.java
@@ -13,10 +13,12 @@ public class Main {
         gd.agregarVertice(5);
         gd.agregarVertice(3);
         gd.agregarVertice(8);
+        gd.agregarVertice(4);
         gd.agregarArco(1,2,"unodos");
         gd.agregarArco(1,3,"unotres");
         gd.agregarArco(2,5,"doscinco");
         gd.agregarArco(3,8,"tresocho");
+        gd.agregarArco(3,4,"trescuatro");
         gd.agregarArco(8,5,"ochocinco");
         Conexion_PC conexion = new Conexion_PC(gd);
         //conexion.camino_a_pc_destino(1,5,2);
